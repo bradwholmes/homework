1c70ab4e6a9d5b054d5cf241cb41c2dcf639bdbe
Brad Holmes <inkspotch@gmail.com>
2015-02-18 23:06:12 -0600

    Init commit of skeleton
    Unit testing framework(robolectric) setup.
    Integration testing(espresso) setup.

---
+++ src/androidTest/java/com/phunware/espresso/activity/HomeScreenTest.java
@@ -0,0 +1,29 @@
+package com.phunware.espresso.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+import com.phunware.R;
+import com.phunware.activity.HomeScreen;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+public class HomeScreenTest extends ActivityInstrumentationTestCase2<HomeScreen> {
+
+    public HomeScreenTest() {
+        super(HomeScreen.class);
+    }
+
+    @Override protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testSaysHello() {
+        onView(withId(R.id.hello))
+                .check(matches(withText(R.string.hello_world)));
+    }
+}
---
+++ src/main/java/com/phunware/activity/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.phunware.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phunware.R;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
