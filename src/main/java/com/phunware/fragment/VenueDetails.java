2e58ffb67b69b3cffef678c8004a1681aead0a95
Brad Holmes <inkspotch@gmail.com>
2015-02-23 18:44:16 -0600

    Add share button to toolbar

--- src/main/java/com/phunware/fragment/VenueDetails.java
+++ src/main/java/com/phunware/fragment/VenueDetails.java
@@ -1,10 +1,11 @@
 package com.phunware.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.*;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 public class VenueDetails extends Fragment {
 
     public static final String KEY_VENUE = "key_venue";
+    private Venue mVenue;
 
     public static Fragment create(Venue venue) {
         Bundle bundle = new Bundle();
@@ -28,6 +30,26 @@ public class VenueDetails extends Fragment {
         VenueDetails fragment = new VenueDetails();
         fragment.setArguments(bundle);
         return fragment;
+    }
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        Bundle arguments = getArguments();
+        if (arguments == null) return;
+
+        mVenue = (Venue) arguments.getSerializable(KEY_VENUE);
+    }
+
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.share, menu);
+
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat
+                .getActionProvider(menu.findItem(R.id.menu_item_share));
+        setupShareAction(shareActionProvider);
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,24 +62,29 @@ public class VenueDetails extends Fragment {
         TextView address = (TextView) view.findViewById(R.id.address);
         ListView listView = (ListView) view.findViewById(R.id.schedule);
 
-        Bundle arguments = getArguments();
-        if(arguments == null) return;
+        if(mVenue == null) return;
 
-        Venue venue = (Venue) arguments.getSerializable(KEY_VENUE);
-        if(venue == null) return;
+        name.setText(mVenue.getName());
+        address.setText(mVenue.getFullAddress());
 
-        name.setText(venue.getName());
-        address.setText(venue.getFullAddress());
-
-        if(!Strings.isNullOrEmpty(venue.getImageUrl())) {
+        if(!Strings.isNullOrEmpty(mVenue.getImageUrl())) {
             Picasso.with(getActivity())
-                    .load(venue.getImageUrl())
+                    .load(mVenue.getImageUrl())
                     .into(photo);
         }
 
-        ArrayList<ScheduleItem> schedule = venue.getSchedule();
+        ArrayList<ScheduleItem> schedule = mVenue.getSchedule();
         if (schedule != null) {
             listView.setAdapter(new ArrayAdapter<>(getActivity(), R.layout.schedule_item, schedule));
         }
+    }
+
+    private void setupShareAction(ShareActionProvider shareActionProvider) {
+        if(mVenue == null || shareActionProvider == null) return;
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, mVenue.getName() + " @ " + mVenue.getFullAddress());
+        shareActionProvider.setShareIntent(shareIntent);
     }
 }
